1f173492e305db2f0c8f72a7a2f1a244f4ef1c6a
konstantin <kkononenko3@gmail.com>
2023-08-30 12:47:45 +0300

    created drf-yasg auto documentations

--- Cinema_API/settings.py
+++ Cinema_API/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     "rest_framework.authtoken",
     "djoser",
     "django_filters",
+    "drf_yasg",
 ]
 
 MIDDLEWARE = [
--- Cinema_API/urls.py
+++ Cinema_API/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .yasg import urlpatterns as doc_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,5 +26,6 @@ urlpatterns = [
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
     path('auth/', include('djoser.urls.jwt')),
-
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += doc_urls
